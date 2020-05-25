2e9c6e0d6a17e999d9f91748542c0fc93ad4dfe9
chienyichi <jijianyi1993@gmail.com>
2020-05-25 14:48:27 +0800

    crop white function update

--- src/preprocess.py
+++ src/preprocess.py
@@ -35,7 +35,7 @@ def save_image():
 
 def save_crop_white():
     orig_path = '/mnt/data/prostate-cancer-grade-assessment/train_images/'
-    save_path = '/mnt/data/prostate-cancer-grade-assessment/train_512_crop_white/'
+    save_path = '/mnt/data/prostate-cancer-grade-assessment/train_level_2_crop_white/'
     os.mkdir(save_path)
 
     train_names = os.listdir(orig_path)
@@ -44,9 +44,9 @@ def save_crop_white():
         img_path = os.path.join(orig_path,t)
         img_save_path = os.path.join(save_path,img_name+'.png')
         image = skimage.io.MultiImage(img_path)
-        image = image[-1]
+        image = image[-2]
         image = crop_white(image)
-        image = cv2.resize(image,(512,512)) 
+        # image = cv2.resize(image,(512,512)) 
         cv2.imwrite(img_save_path,image)    
 
 
@@ -66,4 +66,5 @@ def train_val_split():
 
 if __name__ == "__main__":
     # train_val_split()
-    save_image()
+    # save_image()
+    save_crop_white()
