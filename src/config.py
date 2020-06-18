147fe4e640f405dea45bbae1d11be2df474f2b57
chienyichi <jijianyi1993@gmail.com>
2020-06-18 14:17:03 +0800

    add more cluster to netvlad

--- src/config.py
+++ src/config.py
@@ -5,15 +5,16 @@ if multi_gpu:
     apex = False
 IMG_SIZE = 256
 num_tiles = 36
+num_cluster = 20
 fold = 0
 fold_csv = './data/folds.csv'
 DATA_PATH = f'/mnt/data/prostate-cancer-grade-assessment/train_images/'
-MODEL_PATH = f'/home/jijianyi/workspace/kaggle/logs/panda-challenge/tiles-eb0-netvlad-reg-{num_tiles}-{IMG_SIZE}-f{fold}/'
+MODEL_PATH = f'/home/jijianyi/workspace/kaggle/logs/panda-challenge/tiles-eb0-netvlad-c{num_cluster}-{num_tiles}-{IMG_SIZE}-f{fold}/'
 
 num_folds = 5
 seed=42
-batch_size = 16
+batch_size = 8
 lr = 3e-4
 num_epoch =30
 num_class = 1
-accumulation_steps = 1#16/batch_size
+accumulation_steps = 16/batch_size
